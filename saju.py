c7984428328d3081de8bfef59608a15b0a9abc75
moneylog <moneylog@naver.com>
2018-07-15 22:16:58 +0900

    Add saju

---
+++ saju.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 21:53:58 2018
+
+@author: potolog
+"""
+
+import numpy as np
+import pandas as pd
+
+saju = pd.read_csv('s:/saju/saju-4.csv', encoding='cp949')
+
+print(saju)
